aa0ba675fb150502ae86cc0918010f5947fc5c86
peshala dushyanthika <pdushyanthika@gmail.com>
2026-07-31 14:42:46 +0530

    panel

---
+++ app/Filament/Resources/PresentationPanels/Pages/CreatePresentationPanel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PresentationPanels\Pages;
+
+use App\Filament\Resources\PresentationPanels\PresentationPanelResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePresentationPanel extends CreateRecord
+{
+    protected static string $resource = PresentationPanelResource::class;
+}
